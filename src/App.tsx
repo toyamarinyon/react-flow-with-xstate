6864780d348c456a323c8f142cc3696afbb62c94
toyamarinyon <toyamarinyon@gmail.com>
2024-05-15 22:26:18 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,33 @@ import "reactflow/dist/style.css";
 import { reactFlowMachine } from "./store";
 
 function Flow() {
-  const [snapshot, send] = useMachine(reactFlowMachine);
-  if (snapshot.value === "loadStore") {
-    return null;
-  }
+	const [snapshot, send] = useMachine(reactFlowMachine);
+	if (snapshot.value === "loadStore") {
+		return null;
+	}
 
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <ReactFlow
-        nodes={snapshot.context.nodes}
-        onNodesChange={(changeNodes) =>
-          send({ type: "nodes.change", changeNodes })
-        }
-        edges={snapshot.context.edges}
-        onEdgesChange={(changeEdges) =>
-          send({ type: "edges.change", changeEdges })
-        }
-        onConnect={(connection) => send({ type: "connect", connection })}
-        onInit={(reactFlowInstance) =>
-          send({ type: "reactFlowInstance.set", reactFlowInstance })
-        }
-        fitView
-      >
-        <Background />
-        <Controls />
-      </ReactFlow>
-    </div>
-  );
+	return (
+		<div style={{ height: "100vh", width: "100vw" }}>
+			<ReactFlow
+				nodes={snapshot.context.nodes}
+				onNodesChange={(changeNodes) =>
+					send({ type: "nodes.change", changeNodes })
+				}
+				edges={snapshot.context.edges}
+				onEdgesChange={(changeEdges) =>
+					send({ type: "edges.change", changeEdges })
+				}
+				onConnect={(connection) => send({ type: "connect", connection })}
+				onInit={(reactFlowInstance) =>
+					send({ type: "reactFlowInstance.set", reactFlowInstance })
+				}
+				fitView
+			>
+				<Background />
+				<Controls />
+			</ReactFlow>
+		</div>
+	);
 }
 
 export default Flow;
